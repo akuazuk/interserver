5dc7d78ec66e189ff86d78616b170ea43a13b9b2
Pavel <akuazuk@gmail.com>
2021-07-06 18:49:09 -0400

    Add config file

---
+++ configuration.php
@@ -0,0 +1,63 @@
+<?php
+class JConfig {
+	var $offline = '0';
+	var $editor = 'none';
+	var $list_limit = '20';
+	var $helpurl = '';
+	var $debug = '0';
+	var $debug_lang = '0';
+	var $sef = '1';
+	var $sef_rewrite = '1';
+	var $sef_suffix = '1';
+	var $feed_limit = '10';
+	var $feed_email = 'author';
+	var $secret = 'VPomK2K7N2biFZio';
+	var $gzip = '1';
+	var $error_reporting = '-1';
+	var $xmlrpc_server = '1';
+	var $log_path = '/etc/home/akuazukc/enio-minsk.com/var/www/advertmaster/data/www/enio-minsk.com/logs';
+	var $tmp_path = '/etc/home/akuazukc/enio-minsk.com/var/www/advertmaster/data/www/enio-minsk.com/tmp';
+	var $live_site = '';
+	var $force_ssl = '0';
+	var $offset = '2';
+	var $caching = '0';
+	var $cachetime = '15';
+	var $cache_handler = 'file';
+	var $memcache_settings = array();
+	var $ftp_enable = '0';
+	var $ftp_host = '127.0.0.1';
+	var $ftp_port = '21';
+	var $ftp_user = 'admin';
+	var $ftp_pass = 'Leto,2010';
+	var $ftp_root = '';
+	var $dbtype = 'mysql';
+	var $host = 'localhost';
+	var $user = 'akuazukc_enio';
+	var $db = 'akuazukc_enio';
+	var $dbprefix = 'jos_';
+	var $mailer = 'mail';
+	var $mailfrom = 'akuazuk@gmail.com';
+	var $fromname = 'Enio-Minsk';
+	var $sendmail = '/usr/sbin/sendmail';
+	var $smtpauth = '0';
+	var $smtpsecure = 'none';
+	var $smtpport = '25';
+	var $smtpuser = '';
+	var $smtppass = '';
+	var $smtphost = 'localhost';
+	var $MetaAuthor = '1';
+	var $MetaTitle = '1';
+	var $lifetime = '180';
+	var $session_handler = 'database';
+	var $password = 'Leto,2010';
+	var $sitename = 'Психолог -  специалист - практик с многолетним опытом работы';
+	var $MetaDesc = 'Нечаев Александр Александрович - специалист - практик с многолетним опытом работы. Звоните: +375 29 675-76-16';
+	var $MetaKeys = 'психолог';
+	var $offline_message = 'Сайт сейчас закрыт на техническое обслуживание. Пожалуйста зайдите позже.';
+	var $display_errors = 'Off';
+	
+}
+
+    
+?>
+
